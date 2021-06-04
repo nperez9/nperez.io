1592853cec3b0ea51820e92f11bce58dccd48cd7
Nicolas <nicolasperez95.a@gmail.com>
2021-06-04 00:24:34 -0300

    feat: brand new version!

---
+++ precache-manifest.f9b74be450c7a157c6b4ceb594d2983e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f6b8fc1ca432b6650d380389ac141a",
+    "url": "https://nperez9.github.io/nperez.io/index.html"
+  },
+  {
+    "revision": "3120f2c508f6b5ea0f29",
+    "url": "https://nperez9.github.io/nperez.io/static/js/2.8f568606.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://nperez9.github.io/nperez.io/static/js/2.8f568606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b718aafb7a661ae52174",
+    "url": "https://nperez9.github.io/nperez.io/static/js/main.7d878e00.chunk.js"
+  },
+  {
+    "revision": "3e77a155b5d1616724a6",
+    "url": "https://nperez9.github.io/nperez.io/static/js/runtime-main.f970553b.js"
+  }
+]);
