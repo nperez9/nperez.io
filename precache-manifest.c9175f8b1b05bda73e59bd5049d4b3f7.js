db913c907731758890cf9cf94ac03eab6aa104e5
Nicolas <nicolasperez95.a@gmail.com>
2024-05-04 12:50:14 -0300

    feat: added stuff

--- precache-manifest.c9175f8b1b05bda73e59bd5049d4b3f7.js
+++ precache-manifest.c9175f8b1b05bda73e59bd5049d4b3f7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffd886397b87983f137623a7d686b23c",
+    "revision": "afc7220653cd40562610c0c330b289b8",
     "url": "https://nperez9.github.io/nperez.io/index.html"
   },
   {
-    "revision": "74bb247f887e74ca15b3",
+    "revision": "e8398fe9f2fdb082142d",
     "url": "https://nperez9.github.io/nperez.io/static/css/main.ceb90faf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://nperez9.github.io/nperez.io/static/js/2.3ed963db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "74bb247f887e74ca15b3",
-    "url": "https://nperez9.github.io/nperez.io/static/js/main.b431bbfc.chunk.js"
+    "revision": "e8398fe9f2fdb082142d",
+    "url": "https://nperez9.github.io/nperez.io/static/js/main.1dd405f7.chunk.js"
   },
   {
     "revision": "d03fad90dc188ba2bd1c",
